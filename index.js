bceb8cd36e9bb9d53aaf54756861e0e73447d76b
Berni <royalflushwt@gmail.com>
2026-09-14 11:26:45 +0300

    Use only Discord and join web service

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import { startBot } from "./src/bot.js";
-import { startWeb } from "./src/web.js";
+import { startJoinServer } from "./src/web.js";
 
 async function main() {
-  await startWeb();
+  await startJoinServer();
   await startBot();
 }
 
